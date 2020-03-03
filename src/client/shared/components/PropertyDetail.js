d0afc8eb38aa7664efdc563b6266a13901da2b26
bandarb <sravanthi@vertics.co>
2020-03-03 09:58:16 +0200

    fix

--- src/client/shared/components/PropertyDetail.js
+++ src/client/shared/components/PropertyDetail.js
@@ -11,8 +11,9 @@ class PropertyDetail extends Component {
     this.state = {
       property: null,
       brokers: null,
-      favorites:null,
-      isFavorite:false
+      favorites: null,
+      isFavorite: false,
+      selected:false
     };
   }
 
@@ -21,115 +22,119 @@ class PropertyDetail extends Component {
     axios.get(`/property/${id}`)
       .then(res => this.setState({ property: res.data }))
     axios.get('/broker')
-      .then(res => this.setState({ brokers: res.data }))   
+      .then(res => this.setState({ brokers: res.data }))
     axios.get('/favorite')
-      .then(res => this.setState({ favorites: res.data }))   
+      .then(res => this.setState({ favorites: res.data }))
   }
-  onFavAdd = () =>{
-    const { property,favorites } = this.state;
-    axios.post("/favorite",{property__c:property.sfid})
-      .then(res=>{
-        if(res.data){
-       alert("Property added to favorites")
+  onFavAdd = () => {
+    const { property, favorites } = this.state;
+    axios.post("/favorite", { property__c: property.sfid })
+      .then(res => {
+        if (res.data) {
+          alert("Property added to favorites")
         }
       })
-      .catch(err=>console.log(err))
+      .catch(err => console.log(err))
 
   }
-  onBrokerClick = (id)=>{
+  onBrokerClick = (id) => {
     this.props.history.push(`/broker/${id}`)
   }
-	renderItem = () =>{
-    const { property, brokers,favorites } = this.state;
-    if(!property||!brokers||brokers.length===0||!favorites||favorites.length===0){
+  changeSelected = () =>{
+    this.setState({selected:true})
+  }
+  renderItem = () => {
+    const { property, brokers, favorites,selected } = this.state;
+    if (!property || !brokers || brokers.length === 0 || !favorites || favorites.length === 0) {
       return null
     }
-    const fav = favorites.find(item=>item.sfid===property.sfid)
-	  const {
-	    picture__c, beds__c, baths__c, price__c, title__c, city__c, state__c, broker__c
-	  } = property;
-	  const relatedBroker = brokers.find(item => item.sfid === broker__c);
-	  const sec2Data = [
-	    {
-	      icon: 'king_bed',
-	      text: 'Bedrooms',
-	      number: beds__c
-	    },
-	    {
-	      icon: 'bathtub',
-	      text: 'Bathrooms',
-	      number: baths__c
-	    },
-	    {
-	      icon: 'local_offer',
-	      text: 'Asking Price',
-	      number: formatter.format(price__c)
-	    }
-	  ];
-	  return (
-  <div className="property__item">
-    <img src={picture__c} />
-    <div className="property__details">
-      <div className="property__details-sec1">
-        <div>
-        <h2 className="title">{title__c}</h2>
-        <p className="desc">
-          {city__c}
-          ,
+    const fav = favorites.find(item => item.sfid === property.sfid)
+    const {
+      picture__c, beds__c, baths__c, price__c, title__c, city__c, state__c, broker__c
+    } = property;
+    const relatedBroker = brokers.find(item => item.sfid === broker__c);
+    const sec2Data = [
+      {
+        icon: 'king_bed',
+        text: 'Bedrooms',
+        number: beds__c
+      },
+      {
+        icon: 'bathtub',
+        text: 'Bathrooms',
+        number: baths__c
+      },
+      {
+        icon: 'local_offer',
+        text: 'Asking Price',
+        number: formatter.format(price__c)
+      }
+    ];
+    
+    return (
+      <div className="property__item">
+        <img src={picture__c} />
+        <div className="property__details">
+          <div className="property__details-sec1">
+            <div>
+              <h2 className="title">{title__c}</h2>
+              <p className="desc">
+                {city__c}
+                ,
           {state__c}
-          -
+                -
           {formatter.format(price__c)}
-        </p>
+              </p>
+            </div>
+            <div onClick={fav ? () => { } : this.onFavAdd}>
+              <i className="material-icons">
+                {fav ? "star" : <label title="Add to favorites" onClick={this.changeSelected}>{selected ? "star" :"star_border"}</label>}
+              </i>
+            </div>
+          </div>
+          <div className="property__details-sec2">
+            {sec2Data.map((item, key) => (
+              <div key={key} className="subSec">
+                <div className="subSec__details">
+                  <i className="material-icons">{item.icon}</i>
+                  <span className="subSec__details-text">{item.text}</span>
+                </div>
+                <span className="subSec__number">{item.number}</span>
+              </div>
+            ))}
+            <div className="broker" onClick={() => this.onBrokerClick(relatedBroker.sfid)}>
+              <div className="broker__image">
+                <img src={relatedBroker.picture__c} alt="brokerImg" />
+              </div>
+              <div className="broker__details">
+                <h2>{relatedBroker.name}</h2>
+                <p>{relatedBroker.title__c}</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div onClick={fav?()=>{}:this.onFavAdd}>
-    <i className="material-icons">
-{fav ?"star":<label title="Add to favorites">star_border</label>}
-</i>
-    </div>
       </div>
-      <div className="property__details-sec2">
-        {sec2Data.map((item, key) => (
-          <div key={key} className="subSec">
-            <div className="subSec__details">
-              <i className="material-icons">{item.icon}</i>
-              <span className="subSec__details-text">{item.text}</span>
-            </div>
-            <span className="subSec__number">{item.number}</span>
-          </div>
-        ))}
-          <div className="broker" onClick={()=>this.onBrokerClick(relatedBroker.sfid)}>
-            <div className="broker__image">
-              <img src={relatedBroker.picture__c} alt="brokerImg" />
-            </div>
-            <div className="broker__details">
-              <h2>{relatedBroker.name}</h2>
-              <p>{relatedBroker.title__c}</p>
-            </div>
-          </div>
-      </div>
-    </div>
-  </div>
-	  );
-	};
+    );
+  };
 
-	render() {
+  render() {
     const { property, brokers, prevPath } = this.state;
-    const {location}=this.props
-	  if (!property || !brokers || brokers.length === 0) {
-	    return null;
-	  }
-	  return (
-  <div className="property">
-    <Heading
-      title={property.title__c}
-      linkTitle="Back"
-      icon="keyboard_arrow_left"
-      link={true}
-    />
-    {this.renderItem()}
-  </div>
-	  );
-	}
+    const { location } = this.props
+    if (!property || !brokers || brokers.length === 0) {
+      return null;
+    }
+    return (
+      <div className="property">
+        <Heading
+          title={property.title__c}
+          linkTitle="Back"
+          icon="keyboard_arrow_left"
+          link={true}
+        />
+        {this.renderItem()}
+      </div>
+    );
+  }
 }
 
 export default withRouter(PropertyDetail);
--- src/server/index.js
+++ src/server/index.js
@@ -11,48 +11,13 @@ app.use(express.static('www'));
 app.use(express.static(path.join('www', 'build')));
 
 app.use(bodyParser.json());
-var connectionString = process.env.DATABASE_URL || 'postgres://postgres:test1234@localhost:5432/salesforce';
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/salesforce';
 
 
 const client = new Pool({
 	connectionString
   });
-client.query(`CREATE TABLE broker__c (
-	id SERIAL NOT NULL,
-	sfid CHARACTER VARYING(18) UNIQUE,
-	name CHARACTER VARYING(80),
-	email__c CHARACTER VARYING(80),
-	phone__c CHARACTER VARYING(40),
-	mobile_phone__c CHARACTER VARYING(40),
-	title__c CHARACTER VARYING(30),
-	picture__c CHARACTER VARYING(255)
-   );`);
 
-   client.query(`CREATE TABLE property__c (
-	id SERIAL NOT NULL,
-	sfid CHARACTER VARYING(18) UNIQUE,
-	name CHARACTER VARYING(80),
-	thumbnail__c CHARACTER VARYING(255),
-	beds__c DOUBLE PRECISION,
-	address__c CHARACTER VARYING(100),
-	baths__c DOUBLE PRECISION,
-	broker__c CHARACTER VARYING(18) REFERENCES broker__c(sfid),
-	description__c CHARACTER VARYING(500),
-	state__c CHARACTER VARYING(20),
-	city__c CHARACTER VARYING(50),
-	zip__c CHARACTER VARYING(10),
-	title__c CHARACTER VARYING(100),
-	picture__c CHARACTER VARYING(255),
-	price__c DOUBLE PRECISION,
-	location__longitude__s DOUBLE PRECISION,
-	location__latitude__s DOUBLE PRECISION
-   );`);
-
-   client.query(`CREATE TABLE favorite__c (
-	id SERIAL NOT NULL,
-	sfid CHARACTER VARYING(18) UNIQUE,
-	property__c CHARACTER VARYING(18) REFERENCES property__c(sfid)
-   );`);
 
 var propertyTable = 'property__c';
 var favoriteTable = 'favorite__c';
@@ -60,7 +25,45 @@ var brokerTable = 'broker__c';
 
 // setup the demo data if needed
 client.query('SELECT * FROM salesforce.broker__c', function(error, data) {
+	console.log(error)
 	if (error !== null) {
+		client.query(`CREATE TABLE broker__c (
+			id SERIAL NOT NULL,
+			sfid CHARACTER VARYING(18) UNIQUE,
+			name CHARACTER VARYING(80),
+			email__c CHARACTER VARYING(80),
+			phone__c CHARACTER VARYING(40),
+			mobile_phone__c CHARACTER VARYING(40),
+			title__c CHARACTER VARYING(30),
+			picture__c CHARACTER VARYING(255)
+		   );`);
+		
+		   client.query(`CREATE TABLE property__c (
+			id SERIAL NOT NULL,
+			sfid CHARACTER VARYING(18) UNIQUE,
+			name CHARACTER VARYING(80),
+			thumbnail__c CHARACTER VARYING(255),
+			beds__c DOUBLE PRECISION,
+			address__c CHARACTER VARYING(100),
+			baths__c DOUBLE PRECISION,
+			broker__c CHARACTER VARYING(18) REFERENCES broker__c(sfid),
+			description__c CHARACTER VARYING(500),
+			state__c CHARACTER VARYING(20),
+			city__c CHARACTER VARYING(50),
+			zip__c CHARACTER VARYING(10),
+			title__c CHARACTER VARYING(100),
+			picture__c CHARACTER VARYING(255),
+			price__c DOUBLE PRECISION,
+			location__longitude__s DOUBLE PRECISION,
+			location__latitude__s DOUBLE PRECISION
+		   );`);
+		
+		   client.query(`CREATE TABLE favorite__c (
+			id SERIAL NOT NULL,
+			sfid CHARACTER VARYING(18) UNIQUE,
+			property__c CHARACTER VARYING(18) REFERENCES property__c(sfid)
+		   );`);
+		   
 		client.query('SELECT * FROM broker__c', function(error, data) {
 		  if (error !== null) {
 			console.log('Loading Demo Data...');
