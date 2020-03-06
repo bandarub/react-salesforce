dc3b3544b15cf1fdd9dbdf9a5091bb8b1c1328f0
bandarb <sravanthi@vertics.co>
2020-03-06 11:24:22 +0200

    update

--- src/client/pages/Properties/index.js
+++ src/client/pages/Properties/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import {withRouter,Link} from 'react-router-dom';
+import ReactList from 'react-list';
 
 import Heading from '../../shared/components/Heading'
 import ListItem from '../../shared/components/ListItem'
 import axios from 'axios';
 
-import {formatter} from '../../constants'
 import {normalizeProperty} from '../../utils'
 
 class Properties extends Component {
@@ -46,20 +46,28 @@ class Properties extends Component {
       if(filteredProperties&&filteredProperties.length===0){
         return<div className="statement">No search results found</div>
       } 
-      console.log(filteredProperties)
       return filteredProperties.map((prop,key)=>
         <ListItem item={normalizeProperty(prop)} key={key} data={prop} onClick={this.onItemSelect}/>
      )
   }
+  renderItem=(index,key)=>{
+    const {filteredProperties} = this.state
+
+    return <ListItem item={normalizeProperty(filteredProperties[index])} key={key} data={filteredProperties[index]} onClick={this.onItemSelect}/>
+  }
   
 
   render() {
-      const {searchInput,properties}=this.state
+      const {searchInput,properties,filteredProperties}=this.state
     return <div className="properties">
     <Heading title="Properties" link={false}/>
     <input value={searchInput} placeholder="Type property title to search...." onChange={this.onFieldChange}/>
-    <div className="properties__list">
-    {this.renderList()}
+    <div style={{overflow: 'auto', maxHeight: "75vh"}}>
+          <ReactList
+            itemRenderer={this.renderItem}
+            length={filteredProperties.length}
+            type='uniform'
+          />
         </div></div>;
   }
 }
