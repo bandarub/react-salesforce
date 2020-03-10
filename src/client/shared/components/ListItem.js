b0194cc64a5e50b0f99d2dab65ad41097253106e
sravanthibandaru <sravanthi@vertics.co>
2020-03-10 13:36:00 +0200

    fix

--- src/client/shared/components/ListItem.js
+++ src/client/shared/components/ListItem.js
@@ -9,21 +9,26 @@ const ListItem = ({ item, onClick, data, from, isFavorite }) => {
     </div>
     <>
       <div className="listItem__details" onClick={() => onClick(data)}>
-        <h2 className="listItem__details-title">{item.name}</h2>
-        <p className="listItem__details-desc">
-          {item.desc}
-        </p>
-        {item.address && <p className="listItem__details-desc">
-          {item.address}
-        </p>}
+        <div>
+          <h2 className="listItem__details-title">{item.name}</h2>
+          <p className="listItem__details-desc">
+            {item.desc}
+          </p>
+          {item.address && <p className="listItem__details-desc">
+            {item.address}
+          </p>}
+        </div>
+        {from === "property" &&
+          <div className="fav">
+            <i className="material-icons">
+              {isFavorite ? "star" : ""}
+            </i>
+          </div>}
       </div>
-      {from === "property" &&
-        <div className="fav">
-          <i className="material-icons">
-            {isFavorite ? "star" : ""}
-          </i>
-        </div>}
+
     </>
+
+
   </div>
 }
 
